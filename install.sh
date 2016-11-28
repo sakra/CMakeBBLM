05bc6d4a043c103af89d08e62ce3c5b305b4be40
Sascha Kratky <skratky@gmail.com>
2016-11-28 21:44:00 +0100

    initial version of CMake BBLM generation scripts

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd "`dirname \"$0\"`"
+
+if [ -n "$(which cmake)" ]; then
+	cmake -P CMakeBBLM.cmake
+elif [ -x /Applications/CMake.app/Contents/bin/cmake ]; then
+	/Applications/CMake.app/Contents/bin/cmake -P CMakeBBLM.cmake
+else
+	echo "CMake is not installed." >&2
+	exit 1
+fi
+
+if [ -d ~/Dropbox/Application\ Support/BBEdit ]; then
+	mkdir -v -p ~/Dropbox/Application\ Support/BBEdit/Language\ Modules
+	cp -f -v cmake.plist ~/Dropbox/Application\ Support/BBEdit/Language\ Modules
+else
+	mkdir -v -p ~/Library/Application\ Support/BBEdit/Language\ Modules
+	cp -f -v cmake.plist ~/Library/Application\ Support/BBEdit/Language\ Modules
+fi
+
+echo "CMake BBEdit language module installed. Please restart BBEdit."
